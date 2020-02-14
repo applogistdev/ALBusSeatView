2fcd67b643679f14be7e8e9389572934f93ec42b
Soner Güler <soner.guler@applogist.com>
2020-02-14 20:12:30 +0300

    DoubleDecker support added

--- Example/ALBusSeatView/MyViewController.swift
+++ Example/ALBusSeatView/MyViewController.swift
@@ -9,18 +9,19 @@
 import UIKit
 import ALBusSeatView
 
+struct SeatStub {
+    let id: String
+    let number: Int
+    let salable: Bool
+    let gender: Bool
+    let hall: Bool
+}
+
 class MyViewController : UIViewController {
     
     var seatView: ALBusSeatView?
     
-    struct SeatStub {
-        let id: Int
-        let salable: Bool
-        let gender: Bool
-        let hall: Bool
-    }
-    
-    var seatList = [SeatStub]()
+    var seatList = [[SeatStub]]()
     var selectedSeatlist = [SeatStub]()
     
     override func loadView() {
@@ -32,10 +33,10 @@ class MyViewController : UIViewController {
         config.soldWomanBGColor = UIColor(red: 1.0, green: 95.0 / 255.0, blue: 95.0 / 255.0, alpha: 1.0)
         config.soldManBGColor = UIColor(red: 61.0 / 255.0, green: 145.0 / 255.0, blue: 1.0, alpha: 1.0)
         config.seatShadowColor = UIColor(red: 146.0 / 255.0, green: 184.0 / 255.0, blue: 202.0 / 255.0, alpha: 0.5)
-        
 //        config.busFrontImage = UIImage(named: "bus-front-view")
-//        config.busFrontImageWidth = 50
-        
+//        config.busFrontImageWidth = 120
+//        config.floorSeperatorImage = UIImage(named: "bus-front-view")
+        config.floorSeperatorWidth = 120
         config.centerHallInfoText = "Tüm koltukları görebilmek için kaydırınız!"
         seatView = ALBusSeatView(withConfig: config)
         seatView?.delegate = self
@@ -43,21 +44,14 @@ class MyViewController : UIViewController {
         
         view.addSubview(seatView!)
         self.view = view
-
-        seatView?.reload()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        (0...70).forEach { (count) in
-            let isHall = (count - 2) % 5 == 0
-            let stub = SeatStub(id: count,
-                                salable: Bool.random(),
-                                gender: Bool.random(),
-                                hall: isHall)
-            seatList.append(stub)
-        }
+        let first = getStubList(total: 45)
+        let second = getStubList(total: 30)
+        seatList = [first,second]
         seatView?.reload()
     }
     
@@ -67,24 +61,34 @@ class MyViewController : UIViewController {
         let vW = view.frame.width
         seatView?.frame = CGRect(x: 0, y: (vH-250)/2, width: vW, height: 250)
     }
+    
+    func getStubList(total: Int) -> [SeatStub] {
+        var list = [SeatStub]()
+        (0...total).forEach { (count) in
+            let isHall = (count - 2) % 5 == 0
+            let stub = SeatStub(id: UUID().uuidString,
+                                number: count,
+                                salable: Bool.random(),
+                                gender: Bool.random(),
+                                hall: isHall)
+            list.append(stub)
+        }
+        return list
+    }
 }
 
 extension MyViewController: ALBusSeatViewDelegate {
     
-    func seatView(_ seatView: ALBusSeatView,
-                  didSelectAtIndex index: Int,
-                  seatType: ALBusSeatType) {
+    func seatView(_ seatView: ALBusSeatView, didSelectAtIndex indexPath: IndexPath, seatType: ALBusSeatType) {
         
-        let stub = seatList[index]
+        let stub = seatList[indexPath.section][indexPath.item]
         selectedSeatlist.append(stub)
         seatView.reload()
     }
     
-    func seatView(_ seatView: ALBusSeatView,
-                  deSelectAtIndex index: Int,
-                  seatType: ALBusSeatType) {
+    func seatView(_ seatView: ALBusSeatView, deSelectAtIndex indexPath: IndexPath, seatType: ALBusSeatType) {
         
-        let stub = seatList[index]
+        let stub = seatList[indexPath.section][indexPath.item]
         selectedSeatlist.removeAll(where: { $0.id == stub.id })
         seatView.reload()
     }
@@ -92,14 +96,14 @@ extension MyViewController: ALBusSeatViewDelegate {
 
 extension MyViewController:  ALBusSeatViewDataSource  {
     
-    func seatView(_ seatView: ALBusSeatView, seatNumberForIndex index: Int) -> String {
-        let stub = seatList[index]
-        return "\(stub.id)"
+    func seatView(_ seatView: ALBusSeatView, seatNumberForIndex indexPath: IndexPath) -> String {
+        let stub = seatList[indexPath.section][indexPath.item]
+        return "\(stub.number)"
     }
     
-    func seatView(_ seatView: ALBusSeatView, seatTypeForIndex index: Int) -> ALBusSeatType {
+    func seatView(_ seatView: ALBusSeatView, seatTypeForIndex indexPath: IndexPath) -> ALBusSeatType {
         
-        let stub = seatList[index]
+        let stub = seatList[indexPath.section][indexPath.item]
         
         if stub.hall { // Hall area
             return .none
@@ -117,11 +121,11 @@ extension MyViewController:  ALBusSeatViewDataSource  {
     }
     
     func numberOfSections(in seatView: ALBusSeatView) -> Int {
-        return 1
+        return seatList.count
     }
     
     func seatView(_ seatView: ALBusSeatView, numberOfSeatInSection section: Int) -> Int {
-        return seatList.count
+        return seatList[section].count
     }
 }
 // Present the view controller in the Live View window
